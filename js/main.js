dc43b38a75b385a9a5d208df6f568e7d9e8089d9
Camila Sáez <cfsaezbravo@gmail.com>
2017-05-16 12:42:06 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.getElementById("demo").addEventListener("click", 
+	function (){
+    alert("Hola mundo!");
+});
+
+function fullLoad(){
+	alert("Esta página se cargó!");
+}
+
+function ratonEncima() {
+	alert("Estas sobre mí");
+}
+
+function ratonNoEncima() {
+	alert("Ya no estás sobre mí");
+}
